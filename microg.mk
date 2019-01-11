2a6f5256d3781134dfced95208db37061e411beb
stephrost <smfrost1023@gmail.com>
2019-01-11 03:05:51 -0500

    Add nanolx-variant microG and Phonesky , nlpBackends , Google Adapters , Bromite , signature-spoofing files, misc. helper scripts

---
+++ microg.mk
@@ -0,0 +1,41 @@
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# echo "$(call inherit-product, vendor/microg/microg.mk)" >> ~/lineage/device/google/muskie/aosp_walleye.mk
+# echo "$(call inherit-product, vendor/microg/microg.mk)" >> ~/lineage/device/google/crosshatch/aosp_blueline.mk
+
+LOCAL_PATH := vendor/microg
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/lib64/libjni_keyboarddecoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libjni_keyboarddecoder.so \
+    $(LOCAL_PATH)/lib64/libjni_latinimegoogle.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libjni_latinimegoogle.so \
+    $(LOCAL_PATH)/etc/permissions/features.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/features.xml \
+    $(LOCAL_PATH)/etc/sysconfig/microg-a5k.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/microg-a5k.xml
+
+PRODUCT_PACKAGES += \
+    Bromite \
+    BroWeb \
+    DroidGuard \
+    FDroid \
+    FDroidPrivileged \
+    GmsCore \
+    GoogleBackupTransport \
+    GoogleCalendarSync \
+    GoogleContactsSync \
+    GsfProxy \
+    OpenWeatherMapProvider \
+    Phonesky \
+    com.google.android.maps \
+    nlpBackendIchnaea \
+    nlpBackendNominatim
---
+++ tmpk/ver.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# this section will change as apps are updated
+# modify the output of 'git status' to format below 
+cp ../BroWeb/arm64_SystemWebView.apk .
+cp ../GmsCore/GmsCore.apk .
+cp ../Phonesky/Phonesky.apk .
+
+
+for i in *.apk; do
+    unzip "${i}" 'AndroidManifest.xml' -d "${i%%.apk}"                                     #partial unzip for androidmanifest
+    java -jar ~/jre/AXMLPrinter2.jar "${i%%.apk}"/AndroidManifest.xml > "${i%%.apk}".xml   #binary -> readable androidmanifest
+    echo | sed -n -e 's/^.*\(android:versionName=\)//p' "${i%%.apk}".xml                   #parse version from readable
+    #echo | awk '/android:versionName=/ {print $0}' < "${i%%.apk}".xml
+    rm -rf "${i%%.apk}".xml "${i%%.apk}" "${i}"                                            #remove .apk, folder, readable
+done
