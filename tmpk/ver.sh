44125427e214d82bf76505e91a8d5e818ca901bc
stephrost <smfrost1023@gmail.com>
2019-04-28 13:48:48 -0400

    Update FDroid -> 1.6 , FDroidPrivileged -> 0.2.9 , GmsCore -> 0.2.6.15181-75 , OpenWeatherMapProvider -> 1.0 , Phonesky -> 14.7.50 , Bromite_arm64_SystemWebView -> 74.0.3729.122

--- tmpk/ver.sh
+++ tmpk/ver.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # this section will change as apps are updated
-# modify the output of 'git status' to format below 
-#cp ../BroWeb/arm64_SystemWebView.apk .
+# modify the output of 'git status' to format below
+cp ../BroWeb/arm64_SystemWebView.apk .
 cp ../GmsCore/GmsCore.apk .
 cp ../Phonesky/Phonesky.apk .
-
+cp ../FDroid/FDroid.apk .
+cp ../FDroidPrivileged/FDroidPrivileged.apk .
+cp ../GmsCore/GmsCore.apk .
+cp ../OpenWeatherMapProvider/OpenWeatherMapProvider.apk .
 
 for i in *.apk; do
     unzip "${i}" 'AndroidManifest.xml' -d "${i%%.apk}"                                     #partial unzip for androidmanifest
