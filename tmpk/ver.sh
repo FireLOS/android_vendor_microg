148f7d5f63502578a5b544045e8234058d7035e8
stephrost <smfrost1023@gmail.com>
2019-06-11 10:59:12 -0400

    improved ver.sh

--- tmpk/ver.sh
+++ tmpk/ver.sh
@@ -5,12 +5,11 @@
 cp ../BroWeb/arm64_SystemWebView.apk .
 cp ../GmsCore/GmsCore.apk .
 cp ../Phonesky/Phonesky.apk .
-cp ../FDroid/FDroid.apk .
+#cp ../FDroid/FDroid.apk .
 
 for i in *.apk; do
-    unzip "${i}" 'AndroidManifest.xml' -d "${i%%.apk}"                                     #partial unzip for androidmanifest
-    java -jar ~/jre/AXMLPrinter2.jar "${i%%.apk}"/AndroidManifest.xml > "${i%%.apk}".xml   #binary -> readable androidmanifest
-    echo | sed -n -e 's/^.*\(android:versionName=\)//p' "${i%%.apk}".xml                   #parse version from readable
-    #echo | awk '/android:versionName=/ {print $0}' < "${i%%.apk}".xml
-    rm -rf "${i%%.apk}".xml "${i%%.apk}" "${i}"                                            #remove .apk, folder, readable
+    unzip -q "${i}" 'AndroidManifest.xml' -d "${i%%.apk}"                                     #partial unzip for androidmanifest
+    java -jar ~/jre/AXMLPrinter2.jar "${i%%.apk}"/AndroidManifest.xml > "${i%%.apk}".xml      #binary -> readable androidmanifest
+    echo "${i%%.apk} -> `sed -n -e 's/^.*\(android:versionName=\)//p' "${i%%.apk}".xml`"      #parse version from readable
+    rm -rf "${i%%.apk}".xml "${i%%.apk}" "${i}"                                               #remove .apk, folder, readable
 done
